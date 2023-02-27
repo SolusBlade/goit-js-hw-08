9fb5ff069acea836e80938d530b09f48ae156df2
SolusBlade <kykysha59fafa@gmail.com>
2023-02-27 21:30:52 +0200

    done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import _ from "lodash";
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', _.throttle(onGetTime, 1000));
+
+function onGetTime() {
+    const currentTime = player.getCurrentTime().then(function(seconds) {
+        // console.log(seconds);
+        const time = Math.round(seconds);
+        localStorage.setItem("videoplayer-current-time", time);
+    }).catch(function(error) {
+        // an error occurred
+    });
+}
+
+function onSetTime() {
+    player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function(seconds) {
+        // seconds = the actual time that the player seeked to
+    }).catch(function(error) {
+        switch (error.name) {
+            case 'RangeError':
+                // the time was less than 0 or greater than the video’s duration
+                break;
+    
+            default:
+                // some other error occurred
+                break;
+        }
+    });
+}
+onSetTime();
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import _ from "lodash";
 
+const formRef = document.querySelector(".feedback-form");
+const emailInputRef = document.querySelector('[name="email"]');
+const messageInputRef = document.querySelector('[name="message"]'); 
+
+let data;
+
+formRef.addEventListener('submit', onFormSubmit);
+formRef.addEventListener('input', _.throttle(onFormInput, 500));
+
+function onFormSubmit(event){
+    event.preventDefault();
+    if (data){
+        console.log(data);
+        localStorage.removeItem("feedback-form-state");
+        formRef.reset();
+    }
+};
+
+function onFormInput(){
+    const email = emailInputRef.value;
+    const message = messageInputRef.value;
+    const formElements = {
+        email,
+        message,
+    };
+    localStorage.setItem("feedback-form-state", JSON.stringify(formElements));
+    data = JSON.parse(localStorage.getItem("feedback-form-state"));
+};
+
+function onFormUpload(){
+    if (data){
+        emailInputRef.value = data.email;
+        messageInputRef.value = data.message;
+    }
+}
+onFormUpload()
